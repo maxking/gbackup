a8faadecbeaa09b7815b8c070a3445b560cdaf38
Abhilash Raj <raj.abhilash1@gmail.com>
2019-01-21 15:21:22 -0800

    Change names and structure to support multiple websites.
    - Change [main] in config to [gitlab] and the name represents the website that
      the config is for.
    - If the config file is not present, exit with error.
    - Create a map of section name and function that can backup the utility.

--- gbackup.py
+++ gbackup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 import configparser
 import time
+import logging
 from pathlib import Path
 
 import gitlab
 
-EXPORT_FINISHED_STATUS = 'finished'
+
+GITLAB_EXPORT_FINISHED_STATUS = 'finished'
+EXPORT_WAIT_TIME = 2
+
+log = logging.getLogger('gbackup')
 
 
 def get_config_file():
@@ -13,7 +18,7 @@ def get_config_file():
 
     A typical project config looks something like this::
 
-        [main]
+        [gitlab]
         server = https://gitlab.com
         token = <persosonal-token>
         group =
@@ -32,30 +37,29 @@ def get_config_file():
 def get_config():
     """Return a config object."""
     config_file = get_config_file()
+    if not config_file:
+        return None
     # First, let's see if there is a config file present.
     config = configparser.ConfigParser()
     config.read(str(config_file))
-    if not 'main' in config:
-        msg = 'No `main` section in config file {}'.format(config_file)
-        raise ValueError(msg)
-    return config['main']
+    return config
 
 
-def get_gitlab_instance(config):
+def gl_get_instance(config):
     """Return an instance of gitlab.Gitlab with proper authentication"""
-    gl = gitlab.Gitlab(config['server'],
-                       private_token=config['token'])
-    return gl
+    return gitlab.Gitlab(
+        config['server'],
+        private_token=config['token'])
 
 
-def download_project(project, config, gl):
+def gl_download_project(project, config, gl):
     """Download the backup of a project on to host."""
     project = gl.projects.get(project.id)
     export = project.exports.create({})
     # Wait for the finished status.
     export.refresh()
-    while export.export_status != EXPORT_FINISHED_STATUS:
-        time.sleep(2)
+    while export.export_status != GITLAB_EXPORT_FINISHED_STATUS:
+        time.sleep(EXPORT_WAIT_TIME)
         export.refresh()
 
     backup_path = Path(
@@ -66,13 +70,13 @@ def download_project(project, config, gl):
 
     backup_file = backup_path / time.strftime('%d-%b-%Y-%H-%M.tar.gz')
 
-    print('Downloading backup for {0} to {1}'.format(
-        project.web_url, backup_file))
+    log.info('Downloading backup for %s to %s',
+             project.web_url, backup_file)
     with open(str(backup_file), 'wb') as fd:
         export.download(streamed=True, action=fd.write)
 
 
-def get_projects(gl, config):
+def gl_get_projects(gl, config):
     """Return a list of all the projects.
 
     This function looks at the config and returns all the projects of a user if
@@ -91,12 +95,30 @@ def get_projects(gl, config):
     return projects
 
 
+def backup_gitlab(config):
+    """Backup Gitlab projects."""
+    gl = gl_get_instance(config)
+    for project in gl_get_projects(gl, config):
+        gl_download_project(project, config, gl)
+
+
 def main():
     """Download all the projects."""
     config = get_config()
-    gl = get_gitlab_instance(config)
-    for project in get_projects(gl, config):
-        download_project(project, config, gl)
+    # If there is no config file, exit.
+    if not config:
+        log.error('No config file. Exiting.')
+        exit(1)
+    SECTION_TO_UTILITY = {
+        'gitlab': backup_gitlab,
+    }
+    # Run backup of each website.
+    for section in config.sections():
+        if section not in SECTION_TO_UTILITY:
+            log.error('Unsupported section [%s] in config', section)
+        # Run the actual backup.
+        log.info('Backing up %s', section)
+        SECTION_TO_UTILITY[section](config[section])
 
 
 if __name__ == '__main__':
